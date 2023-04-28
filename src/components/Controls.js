da03116d2205725663513c2b318af9d042a4075f
Brandon <brandon_noel@live.ca>
2023-04-27 22:04:03 -0230

    initial commit

---
+++ src/components/Controls.js
@@ -0,0 +1,10 @@
+const Controls = ({title}) => {
+    console.log(title);
+    return (
+        <>
+            <p>{title}</p>
+            <p>test</p>
+        </>
+    )
+}
+export default Controls;
